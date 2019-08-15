d5cb36feecb10d3dcdce8c82a5a8e894e0e15804
Benoit Koenig <koenigbenoit47@gmail.com>
2019-08-15 15:07:34 -0600

    Add eslint and fix singlequotes and tabs size errors

--- median-of-two-sorted-arrays.js
+++ median-of-two-sorted-arrays.js
@@ -5,80 +5,80 @@
  */
 
 const handleEdgeCase = (total_index, nums1, nums2) => {
-    "Edge cases to be handled before getKthElement starts looping. Handles cases around 0 or list.length"
-    if (nums1.length === 0) {
-        return [nums2[total_index], nums2[total_index + 1]];
+  'Edge cases to be handled before getKthElement starts looping. Handles cases around 0 or list.length'
+  if (nums1.length === 0) {
+    return [nums2[total_index], nums2[total_index + 1]];
+  }
+  if ((nums1.length > total_index) && (nums1[total_index] < nums2[0])) {
+    let k_plus_one_element = nums2[0];
+    if ((nums1.length > total_index + 1) && (nums1[total_index + 1] < nums2[0])) {
+      k_plus_one_element = nums1[total_index + 1];
     }
-    if ((nums1.length > total_index) && (nums1[total_index] < nums2[0])) {
-        let k_plus_one_element = nums2[0];
-        if ((nums1.length > total_index + 1) && (nums1[total_index + 1] < nums2[0])) {
-            k_plus_one_element = nums1[total_index + 1];
-        }
-        return [nums1[total_index], k_plus_one_element];
-    }
-    if ((nums1.length <= nums2.length) && (nums1[nums1.length - 1] < nums2[total_index - nums1.length])) {
-        return [nums2[total_index - nums1.length], nums2[total_index - nums1.length + 1]];
-    }
-    if ((nums1[nums1.length - 1] >= nums2[total_index - nums1.length]) && (nums1[nums1.length - 1] <= nums2[total_index - nums1.length + 1])) {
-        return [nums1[nums1.length - 1], nums2[total_index - nums1.length + 1]];
-    }
-    return false;
+    return [nums1[total_index], k_plus_one_element];
+  }
+  if ((nums1.length <= nums2.length) && (nums1[nums1.length - 1] < nums2[total_index - nums1.length])) {
+    return [nums2[total_index - nums1.length], nums2[total_index - nums1.length + 1]];
+  }
+  if ((nums1[nums1.length - 1] >= nums2[total_index - nums1.length]) && (nums1[nums1.length - 1] <= nums2[total_index - nums1.length + 1])) {
+    return [nums1[nums1.length - 1], nums2[total_index - nums1.length + 1]];
+  }
+  return false;
 }
 
 const getKthAndKPlusOnethElement = (total_index, nums1, nums2, skip_k_plus_one=false) => {
-    "Get the Kth element in the sorted concatenation of nums1, nums2"
-    const edgeCase1 = handleEdgeCase(total_index, nums1, nums2);
-    if (edgeCase1 !== false) {
-        return edgeCase1;
+  'Get the Kth element in the sorted concatenation of nums1, nums2'
+  const edgeCase1 = handleEdgeCase(total_index, nums1, nums2);
+  if (edgeCase1 !== false) {
+    return edgeCase1;
+  }
+  const edgeCase2 = handleEdgeCase(total_index, nums2, nums1);
+  if (edgeCase2 !== false) {
+    return edgeCase2;
+  }
+  const i1RangeValues = [Math.max(total_index - nums2.length + 1, 0), Math.min(total_index, nums1.length)];
+  while (true) {
+    const i1 = parseInt((i1RangeValues[0] + i1RangeValues[1]) / 2);
+    const i2 = total_index - i1;
+    if ((nums1[i1] <= nums2[i2]) && (nums1[i1 + 1] >= nums2[i2 - 1])) {
+      const candidates = [nums1[i1], nums1[i1+1], nums2[i2-1], nums2[i2]].sort((a, b) => (a - b));
+      const kthElement = candidates[1];
+      if (skip_k_plus_one) {
+        return [kthElement];
+      }
+      let kPlusOneElement = candidates[2];
+      if ((nums1[i1 + 2] >= kthElement) && (nums1[i1 + 2] < kPlusOneElement)) {
+        kPlusOneElement = nums1[i1 + 2];
+      }
+      if ((nums2[i2 + 1] >= kthElement) && (nums2[i2 + 1] < kPlusOneElement)) {
+        kPlusOneElement = nums2[i2 + 1];
+      }
+      return [kthElement, kPlusOneElement];
     }
-    const edgeCase2 = handleEdgeCase(total_index, nums2, nums1);
-    if (edgeCase2 !== false) {
-        return edgeCase2;
+    if (nums1[i1] > nums2[i2]) {
+      i1RangeValues[1] = i1;
+    } else {
+      i1RangeValues[0] = i1;
     }
-    const i1RangeValues = [Math.max(total_index - nums2.length + 1, 0), Math.min(total_index, nums1.length)];
-    while (true) {
-        const i1 = parseInt((i1RangeValues[0] + i1RangeValues[1]) / 2);
-        const i2 = total_index - i1;
-        if ((nums1[i1] <= nums2[i2]) && (nums1[i1 + 1] >= nums2[i2 - 1])) {
-            const candidates = [nums1[i1], nums1[i1+1], nums2[i2-1], nums2[i2]].sort((a, b) => (a - b));
-            const kthElement = candidates[1];
-            if (skip_k_plus_one) {
-                return [kthElement];
-            }
-            let kPlusOneElement = candidates[2];
-            if ((nums1[i1 + 2] >= kthElement) && (nums1[i1 + 2] < kPlusOneElement)) {
-                kPlusOneElement = nums1[i1 + 2];
-            }
-            if ((nums2[i2 + 1] >= kthElement) && (nums2[i2 + 1] < kPlusOneElement)) {
-                kPlusOneElement = nums2[i2 + 1];
-            }
-            return [kthElement, kPlusOneElement];
-        }
-        if (nums1[i1] > nums2[i2]) {
-            i1RangeValues[1] = i1;
-        } else {
-            i1RangeValues[0] = i1;
-        }
-    }
+  }
 }
 
 const findMedianSortedArrays = (nums1, nums2) => {
-    "Gets the median of the sorted concatenation of nums1 and nums2"
-    if ((nums1.length === 1) && (nums2.length === 1)) {
-        return .5 * (nums1[0] + nums2[0]);
-    }
-    if ((nums1.length + nums2.length) % 2 === 1) {
-        const median_index = parseInt((nums1.length + nums2.length) / 2);
-        const median = getKthAndKPlusOnethElement(median_index, nums1, nums2, skip_k_plus_one=true);
-        return median[0];
-    } else {
-        const median_low_index = parseInt((nums1.length + nums2.length) / 2) - 1;
-        const median = getKthAndKPlusOnethElement(median_low_index, nums1, nums2);
-        return .5 * (median[0] + median[1]);
-    }
+  'Gets the median of the sorted concatenation of nums1 and nums2'
+  if ((nums1.length === 1) && (nums2.length === 1)) {
+    return .5 * (nums1[0] + nums2[0]);
+  }
+  if ((nums1.length + nums2.length) % 2 === 1) {
+    const median_index = parseInt((nums1.length + nums2.length) / 2);
+    const median = getKthAndKPlusOnethElement(median_index, nums1, nums2, skip_k_plus_one=true);
+    return median[0];
+  } else {
+    const median_low_index = parseInt((nums1.length + nums2.length) / 2) - 1;
+    const median = getKthAndKPlusOnethElement(median_low_index, nums1, nums2);
+    return .5 * (median[0] + median[1]);
+  }
 }
 
 module.exports = {
-    getKthAndKPlusOnethElement,
-    findMedianSortedArrays,
+  getKthAndKPlusOnethElement,
+  findMedianSortedArrays,
 };
--- regular-expression-matching.test.js
+++ regular-expression-matching.test.js
@@ -1,27 +1,27 @@
-const { isMatch } = require("./regular-expression-matching.js");
+const { isMatch } = require('./regular-expression-matching.js');
 
-describe("test regexp matching", () => {
-    it("should match valid matches", () => {
-        expect(isMatch("abcd", ".*")).toBe(true);
-        expect(isMatch("abcd", "ab.*")).toBe(true);
-        expect(isMatch("abcd", "a.*cd")).toBe(true);
-        expect(isMatch("abcd", "a.*bcd")).toBe(true);
-        expect(isMatch("abcd", ".*")).toBe(true);
-        expect(isMatch("abcd", "a*bcd")).toBe(true);
-        expect(isMatch("abcd", "aa*bcd")).toBe(true);
-        expect(isMatch("abcd", "ab.d")).toBe(true);
-        expect(isMatch("abcd", "a..d")).toBe(true);
-        expect(isMatch("a", "ab*")).toBe(true);
-    });
+describe('test regexp matching', () => {
+  it('should match valid matches', () => {
+    expect(isMatch('abcd', '.*')).toBe(true);
+    expect(isMatch('abcd', 'ab.*')).toBe(true);
+    expect(isMatch('abcd', 'a.*cd')).toBe(true);
+    expect(isMatch('abcd', 'a.*bcd')).toBe(true);
+    expect(isMatch('abcd', '.*')).toBe(true);
+    expect(isMatch('abcd', 'a*bcd')).toBe(true);
+    expect(isMatch('abcd', 'aa*bcd')).toBe(true);
+    expect(isMatch('abcd', 'ab.d')).toBe(true);
+    expect(isMatch('abcd', 'a..d')).toBe(true);
+    expect(isMatch('a', 'ab*')).toBe(true);
+  });
 
-    it("should not match invalid matches", () => {
-        expect(isMatch("abcd", "b.*")).toBe(false);
-        expect(isMatch("abcd", "ab.")).toBe(false);
-        expect(isMatch("abcd", ".*dc")).toBe(false);
-        expect(isMatch("aabcd", ".bcd")).toBe(false);
-        expect(isMatch("aabcd", "aaa.*bcd")).toBe(false);
-        expect(isMatch("abcd", "a.bc")).toBe(false);
-        expect(isMatch("abcd", "ab.cd")).toBe(false);
-        expect(isMatch("a", ".*..a*")).toBe(false);
-    });
+  it('should not match invalid matches', () => {
+    expect(isMatch('abcd', 'b.*')).toBe(false);
+    expect(isMatch('abcd', 'ab.')).toBe(false);
+    expect(isMatch('abcd', '.*dc')).toBe(false);
+    expect(isMatch('aabcd', '.bcd')).toBe(false);
+    expect(isMatch('aabcd', 'aaa.*bcd')).toBe(false);
+    expect(isMatch('abcd', 'a.bc')).toBe(false);
+    expect(isMatch('abcd', 'ab.cd')).toBe(false);
+    expect(isMatch('a', '.*..a*')).toBe(false);
+  });
 });
