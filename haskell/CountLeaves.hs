da6e37b2a7e518599873481f79bd86fcccc64afd
Benoit Koenig <koenigbenoit47@gmail.com>
2020-09-13 22:21:21 +0200

    [haskell] Fixed countLeaves, I had it misunderstood

--- haskell/CountLeaves.hs
+++ haskell/CountLeaves.hs
@@ -4,7 +4,8 @@ data Tree a = Empty | Branch a (Tree a) (Tree a)
   deriving (Show, Eq)
 
 countLeaves :: Eq a => Tree a -> Int
-countLeaves Empty = 1
+countLeaves Empty = 0
+countLeaves (Branch _ Empty Empty) = 1
 countLeaves (Branch _ a b) = countLeaves a + countLeaves b
 
 main :: IO ()
